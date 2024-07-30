63fee63e3d4d7bba2d8b3283433d29553af43e86
Fakile Razaq <razaqjimmie91@gmail.com>
2024-07-30 18:03:37 +0100

    Add some conditions

--- quizgame.py
+++ quizgame.py
@@ -10,19 +10,31 @@ score = 0
 
 answer = input("What does CPU stands for? ")
 if answer.lower() == "central processing unit":
+    print("Correct!")
     score += 1
+else:
+    print("Incorrect!")
     
 answer = input("What does GPU stands for? ")
 if answer.lower() == "graphical processing unit":
+    print("Correct!")
     score += 1
+else:
+    print("incorrect!")
     
 answer = input("What does ROM stands for? ")
 if answer.lower() == "read only memory":
+    print("Correct!")
     score += 1
+else:
+    print("Incorrect!")
     
 answer = input("What does AWS stands for? ")
 if answer.lower() == "amazon web services":
+    print("Correct!")
     score += 1
+else:
+    print("Incorrect!")
     
 average = (score / 4) * 100
 
